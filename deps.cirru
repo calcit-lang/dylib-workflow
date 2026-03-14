4134afe1b831cd8e846ccedcc86bf1d67e18b0c4
tiye <jiyinyiyong@gmail.com>
2026-03-14 14:07:02 +0800

    chore: update dylib workflow for latest runtime

--- deps.cirru
+++ deps.cirru
@@ -1,3 +1,3 @@
 
 {}
-  :calcit-version |0.9.9
+  :calcit-version |0.12.9
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
 use cirru_edn::Edn;
 use std::path::Path;
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub fn abi_version() -> String {
   String::from("0.0.9")
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
+pub fn edn_version() -> String {
+  cirru_edn::version().to_string()
+}
+
+#[unsafe(no_mangle)]
 pub fn path_exists(args: Vec<Edn>) -> Result<Edn, String> {
   if args.len() == 1 {
     if let Edn::Str(name) = &args[0] {
